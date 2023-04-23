c96319d200ee8f6dc3ab9029c8d0ea83e50efaf6
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-04-23 23:23:00 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	r.Run()
+}
